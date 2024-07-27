b95e156356c15e7ee989654b134e04072ead0c31
Rakesh8086 <rakeshteja7007@gmail.com>
2024-07-27 20:45:46 +0530

    contains separate functions different prompts that is to be given to the LLM.

--- prompts_to_llm.py
+++ prompts_to_llm.py
@@ -14,7 +14,7 @@ def combined_analysis_of_every_page_prompt():
         "1. Identify the list of subjects that were asked along with the most commonly asked topics in each subject."
         "2. From each subject, find out sub-topics that were most asked or has the chances of being asked in the "
         "upcoming exam."
-        "3. The template should be Subject name - sub-topic1, subtopic2, ..."
+        "3. The template should be Subject name - sub-topic1, subtopic2, .."
         "4. Explain 5 topics on where more attention is to be given and 5 potential topics that can be expected that "
         "were not asked."
         "Make sure that you do not give any additional explanation, answers, notes, suggestions, advices etc and give "

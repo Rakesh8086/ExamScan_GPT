8a7f4f91bcadaf128e6b65a1e493e63365a80791
Rakesh8086 <rakeshteja7007@gmail.com>
2024-07-27 22:30:11 +0530

    Shifted the part of analysis question paper that contained many other function calls to a new function called perform_analysis for be better code reuse. now the app function looks clean with only one function call 'perform_analysis'. The same function can be used in other parts as well, like while generating mock test which first requires analysis of paper.

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
 import streamlit as st
-from temporary_file_creation import save_images_temporarily
-from temporary_file_creation import load_images_from_temporary_folder
-from prompts_to_llm import analyse_page_by_page_prompt
-from prompts_to_llm import combined_analysis_of_every_page_prompt
 import google.generativeai as genai
-from llm_interaction import provide_images_and_extract_topics_from_llm
-from llm_interaction import generate_llm_text_response
-from support_file import combine_responses_and_prompt_as_one
-from support_file import to_markdown
+from support_file import perform_analysis
 
 llm_model = genai.GenerativeModel('gemini-1.5-flash')
 
@@ -87,17 +80,7 @@ def show_analyse_question_paper():
         uploaded_paper_one = st.file_uploader("Upload your Question Paper 1", type=["pdf"])
         if st.button("Analyze Paper"):
             if uploaded_paper_one is not None:
-                path_of_images_of_paper_one, folder_path = save_images_temporarily(uploaded_paper_one,
-                                                                                   pages_to_be_processed)
-                load_saved_images = load_images_from_temporary_folder(folder_path)
-                prompt_for_analysing_paper = analyse_page_by_page_prompt()
-                topic_names = provide_images_and_extract_topics_from_llm(load_saved_images, prompt_for_analysing_paper,
-                                                                         llm_model)
-                prompt_for_analysing_combined_responses = combined_analysis_of_every_page_prompt()
-                responses_and_prompt_combined = combine_responses_and_prompt_as_one(topic_names,
-                                                                                prompt_for_analysing_combined_responses)
-                combined_analysis_for_user = generate_llm_text_response(llm_model, responses_and_prompt_combined)
-                analysis_of_question_paper = to_markdown(combined_analysis_for_user)
+                analysis_of_question_paper = perform_analysis(uploaded_paper_one, pages_to_be_processed, llm_model)
                 st.markdown(analysis_of_question_paper)
                 pass
             else:
@@ -126,6 +109,13 @@ def show_analyse_question_paper():
 def show_generate_mock_test():
     st.header("Generate Mock Test")
     st.subheader("Upload question paper and get mock test.")
+
+    uploaded_model_paper = st.file_uploader("Upload model Question Paper", type=["pdf"])
+    if st.button("Generate mock test"):
+        if uploaded_model_paper is not None:
+            pass
+        else:
+            st.error("Please upload a PDF file.")
 
 
 def show_generate_mind_map():

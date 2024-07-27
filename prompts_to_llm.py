e95a961fa56afbcad7c9243f559ffa99302e058b
Rakesh8086 <rakeshteja7007@gmail.com>
2024-07-27 23:58:58 +0530

    Added a new function that returns a text prompt that is to be used while generating questions using topics extracted. Different prompts are set for generating MCQ and Descriptive questions.

--- prompts_to_llm.py
+++ prompts_to_llm.py
@@ -1,23 +1,72 @@
 def analyse_page_by_page_prompt():
-    analyse_paper_text_prompt = (
+    text_prompt = (
         "Please perform the following tasks by using the image provided:\n\n"
         "1. Identify the key topics involved in each question and categorize its difficulty into easy, medium, hard."
         "Make sure that you do not give any additional explanation, notes, suggestions, advices etc and give the "
         "answer straight to the point as asked."
     )
-    return analyse_paper_text_prompt
+    return text_prompt
 
 
 def combined_analysis_of_every_page_prompt():
-    combined_analysis_text_prompt = (
+    text_prompt = (
         "Please perform the following tasks by using the content provided, knowing that it is a competitive exam:\n\n"
-        "1. Identify the list of subjects that were asked along with the most commonly asked topics in each subject."
-        "2. From each subject, find out sub-topics that were most asked or has the chances of being asked in the "
-        "upcoming exam."
-        "3. The template should be Subject name - sub-topic1, subtopic2, .."
-        "4. Explain 5 topics on where more attention is to be given and 5 potential topics that can be expected that "
+        "1. Identify the list of subjects(like history, mathematics, machine learning) that were asked along with the "
+        "most commonly asked topics in each subject."
+        "2. From each subject, find out 3 sub-topics that were most asked or has the chances of being asked in the "
+        "upcoming exam. The template should be Subject name - sub-topic1, subtopic2, .."
+        "3. Identify the topic that contains most number of questions and had more weightage."
+        "4. Identify the topic that contains most number of easy questions and the topic with most number of hard "
+        "questions"
+        "5. Explain 3 topics on where more attention is to be given and 5 potential topics that can be expected that "
         "were not asked."
-        "Make sure that you do not give any additional explanation, answers, notes, suggestions, advices etc and give "
-        "the answer straight to the point as asked."
+        "Make sure that you do not consider Examination Instructions, Exam format in analysis and do not give any "
+        "additional explanation, answers, notes, suggestions, advices etc and give"
+        "the answer straight to the point as asked. Give title as Analysis of Given Paper."
     )
-    return combined_analysis_text_prompt
+    return text_prompt
+
+
+def analyse_paper_while_generating_mock_test_prompt():
+    text_prompt = (
+        "Please perform the following tasks by using the image provided:\n\n"
+        "1. Identify the potential adjacent topics involved in each question and categorize its difficulty into easy, "
+        "medium, hard, challenging."
+        "Make sure that you do not give any additional explanation, notes, suggestions, advices etc and give the "
+        "answer straight to the point as asked."
+    )
+
+    return text_prompt
+
+
+def generate_questions_prompt(mock_test_type):
+    if mock_test_type == "Multiple-choice":
+        text_prompt = (
+            "Please perform the following tasks by using the content provided and knowing that it is a competitive "
+            "exam:\n\n"
+            "1. Generate the Multiple choice questions with 4 options from the adjacent topics given here with the "
+            "inflated difficulty than the one provided."
+            "2. Make sure that the questions can test the in-depth understanding of the student."
+            "3. Make sure that questions are not direct but requires understanding of the subject"
+            "4. Ensure to properly display questions and not as html tags. use markdown formatting."
+            "5. Do not display topic names and difficulty of generated questions."
+            "Make sure that you do not give any additional explanation, answers, notes, suggestions, advices etc and "
+            "give"
+            "the answer straight to the point as asked."
+        )
+    elif mock_test_type == "Descriptive":
+        text_prompt = (
+            "Please perform the following tasks by using the content provided and knowing that it is a competitive "
+            "exam:\n\n"
+            "1. Generate the Descriptive and Numerical questions from the adjacent topics given here with the "
+            "inflated difficulty than the one provided."
+            "2. Make sure that the questions can test the in-depth understanding of the student."
+            "3. Make sure that questions are not direct but requires understanding of the subject"
+            "4. Ensure to properly display questions and not as html tags. use markdown formatting."
+            "5. Do not display topic names and difficulty of generated questions."
+            "Make sure that you do not give any additional explanation, answers, notes, suggestions, advices etc and "
+            "give"
+            "the answer straight to the point as asked. Title it is Mock Test"
+        )
+
+    return text_prompt

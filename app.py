907abef6a3cf864254ddec4ccdf0c94d44f4637c
Rakesh8086 <rakeshteja7007@gmail.com>
2024-07-31 22:42:18 +0530

    As of now, generate topic specific questions is working well and fully functional. Removed analyse syllabus feature as it seemed unnecessary as any other LLM can do it. Removed analyse three papers at once feature as well. Might have to add it in the future. Added a point in home page note that our app can make mistakes.

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from support_file import combine_analysis_of_two_papers_and_prompt
 from support_file import to_markdown
 from support_file import generate_mind_map_data
 from support_file import perform_analysis_in_detail
+from support_file import generate_topic_specific_questions
 from llm_interaction import generate_llm_text_response
 from prompts_to_llm import analysis_of_multiple_papers_prompt
 from mind_map_generator import generate_mind_map_from_list
@@ -18,7 +19,8 @@ llm_model = genai.GenerativeModel('gemini-1.5-flash')
 def main():
     st.sidebar.header("Navigation")
     page = st.sidebar.selectbox("Choose a page", ["Home", "Analyse Question Paper as Whole", "Page by Page Analysis",
-                                                  "Generate Mock Test", "Generate Mind Map", "Analyse Syllabus"])
+                                                  "Generate Mock Test", "Generate Topic specific Questions",
+                                                  "Generate Mind Map"])
 
     if page == "Home":
         show_home()
@@ -28,14 +30,14 @@ def main():
         show_page_by_page_analysis()
     elif page == "Generate Mock Test":
         show_generate_mock_test()
+    elif page == "Generate Topic specific Questions":
+        show_generate_topic_specific_questions()
     elif page == "Generate Mind Map":
         show_generate_mind_map()
-    elif page == "Analyse Syllabus":
-        show_analyse_syllabus()
 
 
 def show_home():
-    st.title("ExamScan-GPT")
+    st.title("ExamScan GPT")
     st.subheader("A Close Quarter Studios Product")
     st.markdown("<h4 style='color: grey;'>Your One stop solution for Costless Exam Preparation</h2>",
                 unsafe_allow_html=True)
@@ -44,7 +46,8 @@ def show_home():
     st.markdown("""
     <ul>
         <li><h5>The processing time for analysing a question paper is approximately 1 minute for 12 pages, increasing proportionally with the number of pages. </h4></li>
-        <li><h5>Generating a mock test can take atleast 1 minute or longer.</h4></li>
+        <li><h5>Generating a mock test can take at least 1 minute or longer.</h4></li>
+        <li><h5>ExamScan GPT can make mistakes. Check important info.</h4></li>
     </ul>
     """, unsafe_allow_html=True)
 
@@ -53,8 +56,7 @@ def show_analyse_question_paper():
     st.header("Analyse Question Paper")
     st.subheader("Upload question paper and get in analysed in detail.")
 
-    number_of_papers = st.sidebar.selectbox("Number of Question Papers to be analysed at once : ", ["One", "Two",
-                                                                                                    "Three"])
+    number_of_papers = st.sidebar.selectbox("Number of Question Papers to be analysed at once : ", ["One", "Two"])
 
     pages_to_be_processed = st.sidebar.selectbox("Process :",
                                                  ["Every page", "Every Even-numbered page", "Every Odd-numbered "
@@ -81,11 +83,13 @@ def show_analyse_question_paper():
             if uploaded_paper_one is not None and uploaded_paper_two is not None:
                 try:
                     analysis_of_question_paper_one = perform_analysis_on_multiple_papers(uploaded_paper_one,
-                                                                                         pages_to_be_processed, llm_model)
+                                                                                         pages_to_be_processed,
+                                                                                         llm_model)
                     st.write("Hang Tight!!")
                     time.sleep(20)
                     analysis_of_question_paper_two = perform_analysis_on_multiple_papers(uploaded_paper_two,
-                                                                                         pages_to_be_processed, llm_model)
+                                                                                         pages_to_be_processed,
+                                                                                         llm_model)
                     combined_analysis_prompt = analysis_of_multiple_papers_prompt()
                     combined_text = combine_analysis_of_two_papers_and_prompt(analysis_of_question_paper_one,
                                                                               analysis_of_question_paper_two,
@@ -100,16 +104,6 @@ def show_analyse_question_paper():
             else:
                 st.error("Please upload a PDF file.")
 
-    if number_of_papers == "Three":
-        uploaded_paper_one = st.file_uploader("Upload your Question Paper 1", type=["pdf"])
-        uploaded_paper_two = st.file_uploader("Upload your Question Paper 2", type=["pdf"])
-        uploaded_paper_three = st.file_uploader("Upload your Question Paper 3", type=["pdf"])
-        if st.button("Analyze Paper"):
-            if uploaded_paper_one is not None and uploaded_paper_two is not None and uploaded_paper_three is not None:
-                pass
-            else:
-                st.error("Please upload a PDF file.")
-
 
 def show_page_by_page_analysis():
     st.header("Page by Page Analysis")
@@ -120,7 +114,8 @@ def show_page_by_page_analysis():
     if st.button("Analyze Paper"):
         if uploaded_paper is not None:
             try:
-                analysis_of_every_question = perform_analysis_in_detail(uploaded_paper, pages_to_be_processed, llm_model)
+                analysis_of_every_question = perform_analysis_in_detail(uploaded_paper, pages_to_be_processed,
+                                                                        llm_model)
                 # st.write(analysis_of_every_question)
                 for image_file, response in analysis_of_every_question.items():
                     st.write(f"Image: {image_file}")
@@ -136,22 +131,22 @@ def show_page_by_page_analysis():
 def show_generate_mock_test():
     st.header("Generate Mock Test")
     st.subheader("Upload question paper and get mock test.")
-    st.write("Once the questions are generated, Press Cltr+P or click on : and press print option to print the mock "
+    st.write("Once the questions are generated, Press CTRL+P or click on : and press print option to print the mock "
              "test.")
 
     pages_to_be_processed = st.sidebar.selectbox("Process :",
-                                                 ["Every page", "Every Even-numbered page", "Every Odd-numbered "
-                                                                                            "page"])
+                                                 ["Every page", "Every Even-numbered page", "Every Odd-numbered page"])
 
-    question_type = st.sidebar.selectbox("Type of questions to be generated :",
-                                         ["Multiple-choice", "Descriptive"])
+    question_type = st.sidebar.selectbox("Type of questions to be generated :", ["Multiple-choice", "Descriptive"])
 
     uploaded_model_paper = st.file_uploader(" ", type=["pdf"])
     if st.button("Generate mock test"):
         if uploaded_model_paper is not None:
             try:
                 generated_questions, total_pages = perform_analysis_while_mock_test_generation(uploaded_model_paper,
-                                                                        question_type, pages_to_be_processed, llm_model)
+                                                                                               question_type,
+                                                                                               pages_to_be_processed,
+                                                                                               llm_model)
                 if total_pages >= 4:
                     introduction_page_skipper = 0
                     for image_file, response in generated_questions.items():
@@ -174,6 +169,26 @@ def show_generate_mock_test():
             st.error("Please upload a PDF file.")
 
 
+def show_generate_topic_specific_questions():
+    st.header("Generate Topic specific Questions")
+    st.subheader("Mention topic name to generate questions.")
+    difficulty_level = st.sidebar.selectbox("Choose Difficulty : ", ["Easy", "Medium", "Hard", "Challenging"])
+    question_type = st.sidebar.selectbox("Type of questions to be generated :", ["Multiple-choice", "Descriptive"])
+    input_topic = st.text_input(" ")
+    if st.button("Generate Questions"):
+        if input_topic.strip():
+            try:
+                generated_questions = generate_topic_specific_questions(input_topic, difficulty_level, question_type,
+                                                                        llm_model)
+                st.write(generated_questions)
+
+            except Exception as e:
+                st.error(f"An unexpected error occurred. Please try again later.")
+
+        else:
+            st.error("Please enter a topic name.")
+
+
 def show_generate_mind_map():
     st.header("Generate Mind Map")
     st.subheader("Mention topic name to generate mind map.")
@@ -190,11 +205,6 @@ def show_generate_mind_map():
 
         else:
             st.error("Please enter a topic name.")
-
-
-def show_analyse_syllabus():
-    st.header("Analyse Syllabus")
-    st.subheader("Upload Syllabus and get Suggestions for streamlined preparation.")
 
 
 if __name__ == "__main__":

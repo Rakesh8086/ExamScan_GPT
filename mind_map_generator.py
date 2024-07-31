384ef768189ad9133bfd3a6b0c7d25a570b57f6d
Rakesh8086 <rakeshteja7007@gmail.com>
2024-08-01 00:19:23 +0530

    Mind map is not being generated after deploying. So made changes in the except statements to print what type of exception is happening.

--- mind_map_generator.py
+++ mind_map_generator.py
@@ -56,7 +56,7 @@ def generate_mind_map_from_list(keywords_list, mind_map_type):
                                    mime="image/png")
 
     except SyntaxError as e:
-        st.error(f"An unexpected error occurred. Please try again.")
+        st.error(f"An unexpected SyntaxError occurred. Please try again.")
 
     except ValueError as e:
-        st.error(f"An unexpected error occurred. Please try again.")
+        st.error(f"An unexpected ValueError occurred. Please try again.")
